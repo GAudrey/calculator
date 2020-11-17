3a47efb7b21c8c24dbbbd4b930c3d7ebbeb1448b
GAudrey <gilmantaudrey.ag@gmail.com>
2020-11-17 16:23:16 +0100

    add % in js

--- operations-calculator.js
+++ operations-calculator.js
@@ -7,19 +7,28 @@ const contentInput = document.querySelector('input');
 
 for (let content of contentButton){
     content.addEventListener('click', () => {
+        
+        // si différent de AC et différent de =
+        if(content.textContent !== 'AC' && content.textContent != '='){
+            // percent of
+            if(content.textContent == '%'){
+                contentInput.value = (contentInput.value)/100;
+            }
+
+            // add each button content but not % and =
+            else{
+                contentInput.value += content.textContent;
+            }
+        }
+        
         // reset to 0
-        if(content.textContent == 'AC'){
+        else if(content.textContent == 'AC'){
             contentInput.value = '';
         }
 
         // result
-        else if(content.textContent == '='){
+        else {
             contentInput.value = safeEval(contentInput.value)
-        }
-
-        // add each button content but not =
-        else{
-            contentInput.value += content.textContent;
         }
     })
 }
