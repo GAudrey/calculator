c925b22c57d32c57f2232c19c8e498906e01f5f0
GAudrey <gilmantaudrey.ag@gmail.com>
2020-11-18 11:52:47 +0100

    reset last push

--- operations-calculator.js
+++ operations-calculator.js
@@ -10,8 +10,8 @@ const contentInput = document.querySelector('input');
 for (let content of contentButton){
     content.addEventListener('click', () => {
         
-        // si différent de AC et différent de =
-        if(content.textContent !== 'AC' && content.textContent != '='){
+        // if not AC and not =
+        if(content.textContent !== 'AC' && content.textContent !== '='){
             // percent of
             if(content.textContent == '%'){
                 contentInput.value = safeEval(contentInput.value)/100;
@@ -26,21 +26,11 @@ for (let content of contentButton){
         // reset to 0
         else if(content.textContent == 'AC'){
             contentInput.value = '';
-            content.disabled = false;
         }
 
         // result
         else{
             contentInput.value = safeEval(contentInput.value);
-            console.log('result')
-                if(content.textContent !== 'AC'){
-                    content.disabled = true;
-                    console.log('disabled true')
-                }
-                else{
-                    content.disabled = false;
-                    console.log('disabled false')
-                }
         }
     })
 }
